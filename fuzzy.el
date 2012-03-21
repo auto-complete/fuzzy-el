13cac35fd2ea4cb826d81adc76f4b1f06e730973
Tomohiro Matsuyama <tomo@cx4a.org>
2012-03-22 07:58:56 +0900

    Quicksilver abbrev scoring.

--- fuzzy.el
+++ fuzzy.el
@@ -33,18 +33,58 @@
   :group 'convenience
   :prefix "fuzzy-")
 
-(defcustom fuzzy-accept-error-rate 0.10
-  "Error threshold."
-  :group 'fuzzy)
-
-(defvar fuzzy-accept-length-difference 2)
-
 
 
 ;;; Utilities
 
-(defun fuzzy-reverse-string (string)
-  (apply 'string (nreverse (append string nil))))
+(defun fuzzy-current-time-float ()
+  (let ((time (current-time)))
+    (+ (* (float (first time))
+          (lsh 2 16))
+       (float (second time))
+       (/ (float (third time))
+          1000000))))
+
+(defmacro* fuzzy-with-stopwatch ((&optional (elapsed-name 'elapsed)) &body body)
+  (declare (indent 1))
+  (let ((start (gensym "START")))
+    `(let ((,start (fuzzy-current-time-float)))
+       (flet ((,elapsed-name () (- (fuzzy-current-time-float) ,start)))
+         ,@body))))
+
+(defun* fuzzy-add-to-list-as-sorted (list-var value &key (test '<) (key 'identity))
+  (let ((list (symbol-value list-var)))
+    (if (or (null list)
+            (funcall test
+                     (funcall key value)
+                     (funcall key (car list))))
+        (set list-var (cons value list))
+      (while (and list
+                  (cdr list)
+                  (funcall test
+                           (funcall key (cadr list))
+                           (funcall key value)))
+        (setq list (cdr list)))
+      (setcdr list (cons value (cdr list))))))
+
+(defmacro* fuzzy-with-timeout ((timeout &optional timeout-result (tick-name 'tick)) &body body)
+  (declare (indent 1))
+  (let ((elapsed (gensym "ELAPSED")))
+    `(catch 'timeout
+       (fuzzy-with-stopwatch (,elapsed)
+         (flet ((,tick-name ()
+                  (when (and ,timeout (< ,timeout (,elapsed)))
+                    (throw 'timeout ,timeout-result))))
+           ,@body)))))
+
+(defun fuzzy-count-matches-in-string (regexp string &optional start end)
+  (setq start (or start 0)
+        end   (or end (length string)))
+  (loop for start = start then (1+ matched)
+        for matched = (let ((case-fold-search nil))
+                        (string-match regexp string start))
+        while (and matched (< (1+ matched) end))
+        count matched))
 
 
 
@@ -95,21 +135,45 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
 ;; Make sure byte-compiled.
 (byte-compile 'fuzzy-jaro-winkler-distance)
 
-
-
-;;; Edit Distance
-
-(defalias 'fuzzy-edit-distance 'fuzzy-jaro-winkler-distance)
+(defalias 'fuzzy-jaro-winkler-score 'fuzzy-jaro-winkler-distance)
 
 
 
 ;;; Fuzzy Matching
 
-(defun* fuzzy-match (s1 s2 &optional (function 'fuzzy-edit-distance))
+(defcustom fuzzy-match-score-function 'fuzzy-jaro-winkler-score
+  "Score function for fuzzy matching."
+  :type 'function
+  :group 'fuzzy)
+
+(defcustom fuzzy-match-accept-error-rate 0.10
+  "Fuzzy matching error threshold."
+  :type 'number
+  :group 'fuzzy)
+
+(defcustom fuzzy-match-accept-length-difference 2
+  "Fuzzy matching length difference threshold."
+  :type 'number
+  :group 'fuzzy)
+
+(defvar fuzzy-match-score-cache
+  (make-hash-table :test 'equal :weakness t))
+
+(defun fuzzy-match-score (s1 s2 function)
+  (let ((cache-key (list function s1 s2)))
+    (or (gethash cache-key fuzzy-match-score-cache)
+        (puthash cache-key
+                 (funcall function s1 s2)
+                 fuzzy-match-score-cache))))
+
+(defun* fuzzy-match (s1 s2 &optional (function fuzzy-match-score-function))
+  "Return t if S1 and S2 are matched. FUNCTION is a function
+scoring between S1 and S2. The score must be between 0.0 and
+1.0."
   (and (<= (abs (- (length s1) (length s2)))
-           fuzzy-accept-length-difference)
-       (>= (funcall function s1 s2)
-           (- 1 fuzzy-accept-error-rate))))
+           fuzzy-match-accept-length-difference)
+       (>= (fuzzy-match-score s1 s2 function)
+           (- 1 fuzzy-match-accept-error-rate))))
 
 
 
@@ -119,9 +183,8 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
   "`all-completions' with fuzzy matching."
   (loop with length = (length string)
         for str in collection
-        for s = (substring str 0 (min (length str)
-                                      (+ length fuzzy-accept-length-difference)))
-        if (fuzzy-match string s)
+	for len = (min (length str) (+ length fuzzy-match-accept-length-difference))
+        if (fuzzy-match string (substring str 0 len))
         collect str))
 
 
@@ -129,18 +192,15 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
 ;;; Fuzzy Search
 
 (defvar fuzzy-search-some-char-regexp
-  (format "\\w\\{0,%s\\}" fuzzy-accept-length-difference))
-(defvar fuzzy-search-cache nil)
-(defvar fuzzy-search-cache-string nil)
+  (format ".\\{0,%s\\}" fuzzy-match-accept-length-difference))
 
 (defun fuzzy-search-regexp-compile (string)
-  (labels ((oddp (n) (eq (logand n 1) 1))
-           (evenp (n) (eq (logand n 1) 0))
-           (opt (n) (regexp-opt-charset
-                     (append (substring string
-                                        (max 0 (- n 1))
-                                        (min (length string) (+ n 2)))
-                             nil))))
+  (flet ((opt (n)
+           (regexp-opt-charset
+            (append (substring string
+                               (max 0 (- n 1))
+                               (min (length string) (+ n 2)))
+                    nil))))
     (concat
      "\\("
      (loop for i below (length string)
@@ -152,37 +212,13 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
            concat c)
      "\\)")))
 
-(defun fuzzy-search-cache-activate ()
-  (setq fuzzy-search-cache (make-hash-table :weakness t))
-  (setq fuzzy-search-cache-string nil))
-
-(defun fuzzy-search-cache-deactive ()
-  (setq fuzzy-search-cache nil)
-  (setq fuzzy-search-cache-string nil))
-
-(defun fuzzy-search-edit-distance (s1 s2)
-  (or (when fuzzy-search-cache
-        (cond ((null fuzzy-search-cache-string)
-               (setq fuzzy-search-cache-string s1))
-              ((not (equal fuzzy-search-cache-string s1))
-               (setq fuzzy-search-cache-string s1)
-               (clrhash fuzzy-search-cache)))
-        (gethash s2 fuzzy-search-cache))
-      (let ((d (fuzzy-edit-distance s1 s2)))
-        (when fuzzy-search-cache
-          (puthash s2 d fuzzy-search-cache))
-        d)))
-
-(defun fuzzy-search-match (s1 s2)
-  (fuzzy-match s1 s2 'fuzzy-search-edit-distance))
-
 (defun fuzzy-search-forward (string &optional bound noerror count)
-  (let* ((regexp (fuzzy-search-regexp-compile string))
-         match-data)
+  (let ((regexp (fuzzy-search-regexp-compile string))
+        match-data)
     (save-excursion
       (while (and (null match-data)
                   (re-search-forward regexp bound t))
-        (if (fuzzy-search-match string (match-string 1))
+        (if (fuzzy-match string (match-string 1))
             (setq match-data (match-data))
           (goto-char (1+ (match-beginning 1))))))
     (when match-data
@@ -200,7 +236,7 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
         (store-match-data nil)
         (goto-char (max (point-min) (- begin (* (length string) 2))))
         (while (re-search-forward regexp end t)
-          (if (fuzzy-search-match string (match-string 1))
+          (if (fuzzy-match string (match-string 1))
               (setq match-data (match-data))
             (goto-char (1+ (match-beginning 1)))))
         (unless match-data
@@ -224,13 +260,11 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
 
 (defun fuzzy-isearch-activate ()
   (setq fuzzy-isearch t)
-  (setq fuzzy-isearch-failed-count 0)
-  (fuzzy-search-cache-activate))
+  (setq fuzzy-isearch-failed-count 0))
 
 (defun fuzzy-isearch-deactivate ()
   (setq fuzzy-isearch nil)
-  (setq fuzzy-isearch-failed-count 0)
-  (fuzzy-search-cache-deactive))
+  (setq fuzzy-isearch-failed-count 0))
 
 (defun fuzzy-isearch ()
   (cond (isearch-word
@@ -267,6 +301,89 @@ http://en.wikipedia.org/wiki/Jaro-Winkler_distance."
   (if fuzzy-isearch
       (setq ad-return-value (concat fuzzy-isearch-message-prefix ad-return-value))
     ad-return-value))
+
+
+
+;;; QuickSilver's Abbreviation Scoring
+
+(defun fuzzy-quicksilver-make-abbrev-regexp (abbrev)
+  (concat "^"
+          (loop for char across (downcase abbrev) concat
+                (format ".*?\\(%s\\)"
+                        (regexp-quote (string char))))))
+
+(defun fuzzy-quicksilver-abbrev-penalty (string skip-start skip-end)
+  (let ((skipped (- skip-end skip-start)))
+    (cond
+     ((zerop skipped) 0)
+     ((string-match "[ \\t\\r\\n_-]+$" (substring string skip-start skip-end))
+      (let ((seps (- (match-end 0) (match-beginning 0))))
+        (+ seps (* (- skipped seps) 0.15))))
+     ((let ((case-fold-search nil))
+        (eq (string-match "[[:upper:]]" string skip-end) skip-end))
+      (let ((ups (let ((case-fold-search nil))
+                   (fuzzy-quicksilver-count-matches-in-string
+                    "[[:upper:]]" string skip-start skip-end))))
+        (+ ups (* (- skipped ups) 0.15))))
+     (t skipped))))
+
+(defun fuzzy-quicksilver-abbrev-score-nocache (string abbrev)
+  (cond
+   ((zerop (length abbrev))             0.9)
+   ((< (length string) (length abbrev)) 0.0)
+   ((let ((regexp (fuzzy-quicksilver-make-abbrev-regexp abbrev))
+          (case-fold-search t))
+      (string-match regexp string))
+    (loop with groups = (cddr (match-data))
+          while groups
+          for prev    = 0 then end
+          for start   = (pop groups)
+          for end     = (pop groups)
+          for matched = (- end start)
+          for skipped = (- start prev)
+          for penalty = (fuzzy-quicksilver-abbrev-penalty string prev start)
+          sum (+ matched (- skipped penalty)) into point
+          finally return
+          (let* ((length (length string))
+                 (rest (- length end)))
+            (/ (+ point (* rest 0.9)) (float length)))))
+   (t 0.0)))
+
+;; Make sure byte-compiled.
+(byte-compile 'fuzzy-quicksilver-abbrev-score-nocache)
+
+(defvar fuzzy-quicksilver-abbrev-score-cache
+  (make-hash-table :test 'equal :weakness t))
+
+(defun fuzzy-quicksilver-abbrev-score (string abbrev)
+  (let ((cache-key (cons string abbrev)))
+    (or (gethash cache-key fuzzy-quicksilver-abbrev-score-cache)
+        (puthash cache-key
+                 (fuzzy-quicksilver-abbrev-score-nocache string abbrev)
+                 fuzzy-quicksilver-abbrev-score-cache))))
+
+(defun* fuzzy-quicksilver-realtime-abbrev-score (list
+                                                 abbrev
+                                                 &key
+                                                 limit
+                                                 timeout
+                                                 (quality 0.7)
+                                                 &aux new-list)
+  (fuzzy-with-timeout (timeout (nreverse new-list))
+    (loop with length = 0
+          for string in list
+          for score = (fuzzy-quicksilver-abbrev-score string abbrev)
+          if (>= score quality) do
+          (fuzzy-add-to-list-as-sorted
+           'new-list (cons string score)
+           :test '<
+           :key 'cdr)
+          (incf length)
+          if (and limit (> length limit)) do
+          (pop new-list)
+          (setq length limit)
+          do (tick)
+          finally return (nreverse new-list))))
 
 (provide 'fuzzy)
 ;;; fuzzy.el ends here
